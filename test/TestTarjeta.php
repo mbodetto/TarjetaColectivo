ad5e96fd7bdd2b173f39bc97158f2dc28f4538f9
mbodetto <matiasbodetto@gmail.com>
2016-09-27 19:14:24 +0200

    Un test mas

--- test/TestTarjeta.php
+++ test/TestTarjeta.php
@@ -1,18 +1,23 @@
 <?php
 
-
 namespace Poli\Tarjeta;
 
 use PHPUnit\Framework\TestCase;
 
 class TarjetaTest extends TestCase {
 
-  public function testCargaSaldo() {
+  public function testCarga1() {
     $tarjeta = new Sube;
     $tarjeta->recargar(272);
     $this->assertEquals($tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
   }
 
+  public function testCarga2 {
+    $tarjeta = new Sube;
+    $tarjeta->recargar(100);
+    $this->assertEquals($tarjeta->saldo(), 100); 
+  }
+  
 
   public function testPagarViaje() {
 
