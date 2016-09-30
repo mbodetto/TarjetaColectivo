7f39ec15afde9d6bea96685d1a7461bfb5b3049b
mbodetto <matiasbodetto@gmail.com>
2016-09-30 15:50:29 -0300

    Update Sube.php

--- src/Sube.php
+++ src/Sube.php
@@ -2,48 +2,47 @@
 
 namespace Tarjeta;
 
-class Sube implements InterfaceTarjeta {
-
-  private $viajes = [];
-  private $saldo;
-  protected $descuento;
-
-  public function __construct() {
-    $this->saldo = 0;
-    $this->descuento = 1;
-  }
-
-  public function pagar(Transporte $transporte, $fecha_y_hora) {
-    if ($transporte->tipo() == "colectivo") {
-      $this->pagarColectivo($transporte, $fecha_y_hora);
-    }
-    else if ($transporte->tipo() == "bici") {
-      $this->viajes[] = new Viaje($transporte->tipo(), 12, $transporte, strtotime($fecha_y_hora));
-      $this->saldo -= 12;
-    }
-  }
-
-  protected function pagarColectivo(Transporte $transporte, $fecha_y_hora) {
-    $trasbordo = FALSE;
-    if (count($this->viajes) > 0) {
-      if (end($this->viajes)->tiempo() - strtotime($fecha_y_hora) < 3600) {
-        $trasbordo = TRUE;
-      }
-    }
-
-    $monto = 0;
-    if ($trasbordo) {
-      $monto = 2.64 * $this->descuento;
-    }
-    else {
-      $monto = 2 * $this->descuento;
-    }
-
-    $this->viajes[] = new Viaje($transporte->tipo(), $monto, $transporte, strtotime($fecha_y_hora));
-    $this->saldo -= $monto;
-  }
-
-  public function recargar($monto){
+class Tarjeta implements Interfaz_Tarjeta{
+	protected $saldo,$porcentaje;
+	protected $viajes,$ultimafecha=0,$ultimabicipaga=0;
+	public function __construct (){
+		$this->saldo = 0;
+		$this->porcentaje = 1;
+	}
+	public function pagar(Transporte $transporte, $fecha_y_hora){
+	if($transporte->getTipo()==1){ 
+		$aux1 = strtotime($fecha_y_hora);
+		$aux2 = strtotime($this->ultimafecha);
+		if($this->ultimafecha == 0 || ($aux1-$aux2>3600) || $this->viajes[$this->ultimafecha]->getTransporte()->getId() == $transporte->getid()){ 
+			$costo = $transporte->getCosto()*$this->porcentaje;
+		} else {
+			$costo = $transporte->getCostoTrans()*$this->porcentaje;
+		}
+		if($costo<=$this->saldo){
+			$this->saldo -= $costo;
+			$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
+			$this->ultimafecha = $fecha_y_hora;
+			return 1;
+		} else {
+			return 0;
+		}
+		} 
+	if($transporte->getTipo()==2){ 
+		$aux1 = strtotime($fecha_y_hora);
+		$aux2 = strtotime($this->ultimabicipaga);
+		
+		if($this->ultimabicipaga == 0 || ($aux1-$aux2>86400)){
+			$costo = $transporte->getCosto();
+			$this->saldo -= $costo;
+			$this->ultimabicipaga = $fecha_y_hora;
+		} else {
+			$costo = 0;
+		}
+		$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
+		return 1;
+	}
+	}
+	public function recargar($monto){
 		if($monto>=500){
 			$monto+=140;
 		} else if($monto>=272){
@@ -51,13 +50,11 @@ class Sube implements InterfaceTarjeta {
 		}
 		$this->saldo+=$monto;
 	}
-
-  public function saldo() {
-    return $this->saldo;
-  }
-
-  public function viajesRealizados() {
-    return $this->viajes;
-  }
+	public function saldo(){
+		return $this->saldo;
+	}
+	public function viajesRealizados(){
+		return $this->viajes;
+	}
 }
 
