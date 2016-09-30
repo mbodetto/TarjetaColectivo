b68a6dea13954359119654a7249b57150881da3e
mbodetto <matiasbodetto@gmail.com>
2016-09-30 18:04:53 -0300

    Update tarjetaTest.php

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -6,11 +6,9 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
   protected $tarjeta,$colectivoA,$colectivoB,$medio,$bici;	
   public function setup(){
 	$this->tarjeta = new Tarjeta();
-      	$this->medio = new Medio();
 	$this->colectivoA = new Colectivo("144 Negro", "Rosario Bus");
   	$this->colectivoB = new Colectivo("135", "Rosario Bus");
-      	$this->biciA = new Bicicleta("323");
-      	$this->biciB = new Bicicleta("111");
+      	$this->bici = new Bicicleta("123");
   }	
   public function testCargaSaldo() {
     	$this->tarjeta->recargar(272);
