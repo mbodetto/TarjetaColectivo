4b9a12efef2f31ad2868926710330e90e65b389d
mbodetto <matiasbodetto@gmail.com>
2016-09-30 15:49:47 -0300

    Update MedioBoleto.php

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -2,10 +2,10 @@
 
 namespace Tarjeta;
 
-class MedioBoleto extends Sube {
-  public function __construct() {
-    $this->saldo = 0;
-    $this->descuento = 0.5;
-  }
+class Medio extends Tarjeta{
+	public function __construct (){
+		$this->saldo = 0;
+		$this->porcentaje = 0.5;
+	}
 }
 
