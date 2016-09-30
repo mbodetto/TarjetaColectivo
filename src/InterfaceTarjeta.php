40751a00641477dab814466950923109d340bdc0
mbodetto <matiasbodetto@gmail.com>
2016-09-30 16:11:49 -0300

    Update InterfaceTarjeta.php

--- src/InterfaceTarjeta.php
+++ src/InterfaceTarjeta.php
@@ -3,12 +3,10 @@
 namespace Tarjeta;
 
 interface InterfaceTarjeta {
-  
  public function pagar(Transporte $transporte, $fecha_y_hora);
-  
  public function recargar($monto);
-  
- public function saldo();
-  
+ public function saldo(); 
  public function viajesRealizados();
 }
+
+?>
