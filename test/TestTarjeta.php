b1838329f9b164c69ba585a35804d0af7c661474
mbodetto <matiasbodetto@gmail.com>
2016-09-30 15:58:17 -0300

    Cambios en el test

--- test/TestTarjeta.php
+++ test/TestTarjeta.php
@@ -4,17 +4,20 @@ namespace Tarjeta;
 
 use PHPUnit\Framework\TestCase;
 
-class TestTarjeta extends TestCase {
+  protected $tarjeta,$medio,$colectivo144,$colectivo35verde,$bici10;	
 
-  public function testCarga1() {
-    $tarjeta = new Sube;
-    $tarjeta->recargar(272);
-    $this->assertEquals($tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
-  }
+  public function setup(){
+			$this->tarjeta = new Tarjeta();
+      $this->medio = new Medio();
+			$this->colectivoA = new Colectivo("144 Rojo", "Rosario Bus");
+  		$this->colectivoB = new Colectivo("35 Verde", "Rosario Bus");
+      $this->bici10 = new Bicicleta("10");
+  }	
 
-  public function testCarga2() {
-    $tarjeta = new Sube;
-    $tarjeta->recargar(100);
-    $this->assertEquals($tarjeta->saldo(), 100); 
+  public function testCargandoTarjeta() {
+    $this->tarjeta->recargar(272);
+    $this->assertEquals($this->tarjeta->saldo(), 320, "Cuando cargo 272 tendría que tener 320 pesos");
+    $this->tarjeta = new Tarjeta();
+    $this->tarjeta->recargar(510);
+    $this->assertEquals($this->tarjeta->saldo(), 650, "Cuando cargo 510 tendría que tener 645 pesos");
   }
-}
