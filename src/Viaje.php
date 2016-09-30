d5a2927d3cbf68ee6af27659cea078aac4d932d5
mbodetto <matiasbodetto@gmail.com>
2016-09-30 15:51:39 -0300

    Update Viaje.php

--- src/Viaje.php
+++ src/Viaje.php
@@ -3,34 +3,23 @@
 namespace Tarjeta;
 
 class Viaje {
-
-  private $tipo;
-
-  private $monto;
-
-  private $transporte;
-
-  private $tiempo;
-
-  public function __construct($tipo, $monto, $transporte, $tiempo) {
-    $this->tipo = $tipo;
-    $this->monto = $monto;
-    $this->transporte = $transporte;
-  }
-
-  public function tipo() {
-    return $this->tipo;
-  }
-
-  public function monto() {
-    return $this->monto;
-  }
-
-  public function transporte() {
-    return $this->transporte;
-  }
-
-  public function tiempo() {
-    return $this->tiempo;
-  }
+	protected $horario,$transporte,$costo,$tipo,$tipos = array(1 => "Viaje en Colectivo" , 2 => "Viaje en Bici");
+	public function __construct($horario,Transporte $transporte,$costo){
+		$this->horario=$horario;
+		$this->transporte=$transporte;
+		$this->costo=$costo;
+		$this->tipo=$transporte->getTipo();
+	}
+	public function getCosto(){
+		return $this->costo;
+	}
+	public function getHorario(){
+		return $this->horario;
+	}
+	public function getTransporte(){
+		return $this->transporte;
+	}
+	public function getTipo(){
+		return $this->tipos[$this->tipo];
+	}
 }
