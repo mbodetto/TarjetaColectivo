3f59779a2f06b716b1a05507dbd274c9a6d12397
mbodetto <matiasbodetto@gmail.com>
2016-09-30 15:48:48 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -2,19 +2,16 @@
 
 namespace Tarjeta;
 
-class Colectivo extends Transporte {
-
-  private $nombre;
-
-  private $linea;
-
-  public function __construct($nombre, $linea) {
-    $this->tipo = "colectivo";
-    $this->nombre = $nombre;
-    $this->linea = $linea;
-  }
-
-  public function Nombre() {
-    return $this->nombre;
-  }
+class Colectivo extends Transporte{
+	protected $empresa;
+	public function __construct($id,$empresa){
+		$this->id=$id;
+		$this->empresa=$empresa;
+		$this->costo=8;
+		$this->costotrans=2.64;
+		$this->tipo=1;
+	}
+	public function getNombreEmpresa(){
+		$this->empresa;
+	}
 }
