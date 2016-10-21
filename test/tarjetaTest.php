e120702a5371125d96be823c0c1b8a004401bd8d
mbodetto <matiasbodetto@gmail.com>
2016-10-22 01:15:46 +0200

    Test fix

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -6,7 +6,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
   	protected $tarjeta,$colectivoA,$colectivoB,$medio,$bici;	
   	
 	public function setup(){
-		$this->tarjeta = new Tarjeta();
+		$this->tarjeta = new Tarjeta(10);
 		$this->colectivoA = new Colectivo("153 Negro", "Rosario Bus");
   		$this->colectivoB = new Colectivo("135", "Rosario Bus");
 		$this->bici = new Bicicleta("444");
@@ -16,7 +16,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 	public function testCargaSaldo() {
     		$this->tarjeta->recargar(272);
     		$this->assertEquals($this->tarjeta->saldo(), 320, "Cuando cargo 272 tengo que tener 320");
-    		$this->tarjeta = new Tarjeta();
+    		$this->tarjeta = new Tarjeta(11);
     		$this->tarjeta->recargar(500);
     		$this->assertEquals($this->tarjeta->saldo(), 640, "Cuando cargo 500 tengo que tener 640");
   	}
@@ -24,6 +24,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 	public function testPagando(){
 		$this->tarjeta->recargar(40);
   		$this->tarjeta->pagar($this->colectivoA, "2016/04/4 10:50");
+		$this->assertEquals($this->tarjeta->pagar($this->colectivoA, "2016/04/4 10:50")->getTipo(),"Normal", "Debe devolver boleto tipo Normal");
   		$this->assertEquals($this->tarjeta->saldo(), 32, "Cargue 40, menos lo que sale el colectivo 32");
 	}
 	
