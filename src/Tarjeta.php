37a792263950666e86943bd96dbe5d0f356a3ba4
mbodetto <matiasbodetto@gmail.com>
2016-10-22 01:03:48 +0200

    Actualizando parte 2

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -15,41 +15,77 @@ class Tarjeta implements InterfaceTarjeta{
 	
 	
 	public function pagar(Transporte $transporte, $fecha_y_hora){
-	if($transporte->getTipo()==1){ 
-		$aux1 = strtotime($fecha_y_hora);
-		$aux2 = strtotime($this->ultimafecha);
-		if($this->ultimafecha == 0 || ($aux1-$aux2>3600) || $this->viajes[$this->ultimafecha]->getTransporte()->getId() == $transporte->getid()){ 
-			$costo = ($transporte->getCosto()*$this->porcentaje);
+		if($transporte->getTipo()==1){ // 1==Colectivo
+			$aux1 = strtotime($fecha_y_hora);
+			$aux2 = strtotime($this->ultimafecha);
+			$dia = $aux1 - $this->lunes;
+			$a = $dia % 86400;
+			$dia = $dia - $a;
+			$dia = ($dia/86400) % 7;
+			
+			if(($dia==5 && $a>50400 && $a<79200) || ($dia==6 && $a>21600 && $a<79200) || $a<21600 || $a>79200 ){
+				$this->tiempomaxtransbordo=5400;
+			} 
+			else{
+				$this->tiempomaxtransbordo=3600;
+			}
+			
+			if($this->ultimafecha == 0 || ($aux1-$aux2>$this->tiempomaxtransbordo) || $this->viajes[$this->ultimafecha]->getTransporte()->getId() == $transporte->getid() || $this->trans==1){
+				$costo = ($transporte->getCosto()*$this->porcentaje);
+				$this->trans=0;
+			} 
+			else{
+				$costo = ($transporte->getCostoTrans()*$this->porcentaje);
+				$this->trans=1;
+				if($costo>$this->saldo){
+					$costo = ($transporte->getCosto()*$this->porcentaje);
+					$this->trans=0;
+				}
+			}
+			
+			if($this->pasajesPlus<2){
+				if($costo>$this->saldo){
+					$this->pasajesPlus++;
+				}
+	
+				$this->saldo -= $costo;
+				$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
+				$this->ultimafecha = $fecha_y_hora;
+				if($this->pasajesPlus!=0){
+					$boleto = new Boleto($fecha_y_hora,2,$costo,$this->saldo,$transporte->getId(),$this->id);
+				} 
+				else if ($this->porcentaje==0.5){
+					$boleto = new Boleto($fecha_y_hora,3,$costo,$this->saldo,$transporte->getId(),$this->id);
+				} 
+					else {
+						$boleto = new Boleto($fecha_y_hora,1,$costo,$this->saldo,$transporte->getId(),$this->id);
+					}
+				return $boleto; // Se paga
+			} 
+			else{
+				return 0;	// No se paga
+			}
 		} 
-		else {
-			$costo = ($transporte->getCostoTrans()*$this->porcentaje);
-		}
-		if($costo<=$this->saldo){
-			$this->saldo -= $costo;
-			$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
-			$this->ultimafecha = $fecha_y_hora;
-			return 1;
-		} 
-		else {
-			return 0;
-		}
-	} 
-	if($transporte->getTipo()==2){ 
-		$aux1 = strtotime($fecha_y_hora);
-		$aux2 = strtotime($this->ultimabicipaga);
+	
+		if($transporte->getTipo()==2){ // 2==Bici
+			$aux1 = strtotime($fecha_y_hora);
+			$aux2 = strtotime($this->ultimabicipaga);
 		
-		if($this->ultimabicipaga == 0 || ($aux1-$aux2>86400)){
-			$costo = $transporte->getCosto();
-			$this->saldo -= $costo;
-			$this->ultimabicipaga = $fecha_y_hora;
-		} 
-		else {
-			$costo = 0;
-		}
+			if($this->ultimabicipaga == 0 || ($aux1-$aux2>86400)){
+				$costo = $transporte->getCosto();
+				$this->saldo -= $costo;
+				$this->ultimabicipaga = $fecha_y_hora;
+			} 
+			else{
+				$costo = 0;
+			}
+		
 		$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
 		return 1;
+		} 
 	}
-	}
+	
+	
 	public function recargar($monto){
 		if($monto>=500){
 			$monto+=140;
@@ -58,6 +94,7 @@ class Tarjeta implements InterfaceTarjeta{
 			$monto+=48;
 		}
 		$this->saldo+=$monto;
+		$this->pasajesPlus=0;
 	}
 	public function saldo(){
 		return $this->saldo;
