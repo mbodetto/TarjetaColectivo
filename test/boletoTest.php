3350c7c394b0a2631f8bba8aa6b66655ebc44c34
mbodetto <matiasbodetto@gmail.com>
2016-10-22 01:44:18 +0200

    Listo

--- test/boletoTest.php
+++ test/boletoTest.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Tarjeta;
+
+class BoletoTest extends \PHPUnit_Framework_TestCase {
+  protected $tarjeta,$colectivoA,$colectivoB,$medio,$bici;	
+  public function setup(){
+			$this->tarjeta = new Tarjeta(1);
+		  $this->colectivoA = new Colectivo("153 Negro", "Rosario Bus");
+  		$this->colectivoB = new Colectivo("153 Rojo", "Rosario Bus");
+  }	
+
+  public function testBoleto() {
+    $this->tarjeta->recargar(272);
+    $aux=$this->tarjeta->pagar($this->colectivoA, "2016/07/4 6:50");
+    $this->assertEquals($aux->getTipo(),"Normal", "Tipo: Normal");
+    $this->assertEquals($aux->getCosto(),8, "Costo del boleto: 8");
+    $this->assertEquals($aux->getLinea(),"153 Negro", "Linea: 153");
+    $this->assertEquals($aux->getFecha(),"2016/07/4 6:50", "Fecha: 2016/07/4 6:50");
+    $this->assertEquals($aux->getId(),23, "La id de la tarjeta es 1");
+    $this->assertEquals($aux->getSaldo(),312, "Tiene 312 de saldo");
+    $this->assertEquals($this->tarjeta->saldo(), 312, "Cuando recargo 272 y pago un colectivo debo tener 312");
+  }
+ 
+}
+?>
